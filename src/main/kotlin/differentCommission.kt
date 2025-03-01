b7bb4284da3304b75be2f8a83f6343c7bc2a9c25
Aleksey Zhukov <jhukov2001@mail.ru>
2025-03-01 23:06:13 +0300

    Thread commit

--- src/main/kotlin/differentCommission.kt
+++ src/main/kotlin/differentCommission.kt
@@ -1,12 +1,12 @@
 fun main() {
 
-    println(calcCommission("Mastercard", 70_000.0, 10_000.0))
+    println(calcCommission(typeCard = "Mastercard", amountTransfer = 70_000.0, 10_000.0))
 }
 
 val limitDay = 150_000
 val limitMonth = 600_000
 
-fun calcCommission(typeCard: String, amountTransfer: Double, sumTransfer: Double): String {
+fun calcCommission(typeCard: String = "Мир", amountTransfer: Double = 0.0, sumTransfer: Double): String {
     when (typeCard) {
         "Mastercard" -> return calcCommissionMaster(amountTransfer, sumTransfer)
         "Visa" -> return calcCommiccionVisa(amountTransfer, sumTransfer)
@@ -17,7 +17,8 @@ fun calcCommission(typeCard: String, amountTransfer: Double, sumTransfer: Double
 }
 
 fun calcCommissionMaster(amountTransfer: Double, sumTransfer: Double): String {
-    var limitInMonth = 75_000.0
+    var noCommissionLimit = 75_000.0
+
     return when {
         amountTransfer + sumTransfer > limitMonth -> {
             "Опеация невозможна, превышен месячный лимит в размере $limitMonth на: " + (amountTransfer + sumTransfer - limitMonth)
@@ -27,17 +28,19 @@ fun calcCommissionMaster(amountTransfer: Double, sumTransfer: Double): String {
             "Операция невозможна, превышен суточный лимит на операцию в размере $limitDay на: " + (sumTransfer - limitDay)
         }
 
-        amountTransfer + sumTransfer <= limitMonth && sumTransfer <= limitDay && limitInMonth - sumTransfer >= 0 && (limitInMonth - sumTransfer >= 0 && limitInMonth > sumTransfer + amountTransfer) -> {
-            limitInMonth -= sumTransfer
-            "Перевод в размере $sumTransfer осуществлен."
-        }
-
         else -> {
-            limitInMonth -= amountTransfer
-            if (limitInMonth < 0) {
-                limitInMonth = 0.0
+            var commission = when {
+                noCommissionLimit <= amountTransfer -> {
+                    sumTransfer * 0.006 + 20
+                }
+
+                noCommissionLimit <= sumTransfer + amountTransfer -> {
+                    ((sumTransfer + amountTransfer) - noCommissionLimit) * 0.006 + 20
+                }
+
+                else -> 0.0
             }
-            var commission = ((sumTransfer - limitInMonth) * 0.006 + 20)
+            //var commission = ((sumTransfer - noCommissionLimit) * 0.006 + 20)
             "Перевод в размере $sumTransfer осуществлен успешно.\n" +
                     "Комиссия за перевод составила: $commission."
         }
